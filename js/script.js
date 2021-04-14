e3748e451a013422e4b335d7f36c26a0f9a4c912
Anar Agaev <anar.n.agaev@gmail.com>
2021-04-14 10:24:32 +0300

    Added responsive picture

--- js/script.js
+++ js/script.js
@@ -1,22 +1,30 @@
 $(document).ready(function () {
-	let dealay = 5;
-	let offsetTop = 0;
 
-	setInterval(function () {
-		if (dealay > 1) {
-			const activeItemIndex = $('.plug__timer-item.active').index() + 1;
+    function timer() {
+        let dealay = 5;
+        let offsetTop = 0;
 
-			offsetTop -= 24;
-			$('.plug__timer-body').css('top', offsetTop + 'px');
-			$('.plug__timer-item').removeClass('active');
-			$('.plug__timer-item').eq(activeItemIndex).addClass('active');
-			
-		} else {
-			// window.location.replace("https://www.ledcapital.ru/");
-			// document.location.href="https://www.ledcapital.ru/";
-		}
+        setInterval(function () {
+            if (dealay > 1) {
+                const activeItemIndex = $('.plug__timer-item.active').index() + 1;
 
-		dealay--;
-	}, 1000);
-	
+                offsetTop -= 24;
+                $('.plug__timer-body').css('top', offsetTop + 'px');
+                $('.plug__timer-item').removeClass('active');
+                $('.plug__timer-item').eq(activeItemIndex).addClass('active');
+                
+            } else {
+                // window.location.replace("https://www.ledcapital.ru/store");
+                // document.location.href="https://www.ledcapital.ru/store";
+            }
+
+            dealay--;
+        }, 1000);
+	}
+
+    // Show content
+    $('.plug').addClass('visible');
+
+    // Start timer and redirect
+    setTimeout(timer, 3000);
 });
